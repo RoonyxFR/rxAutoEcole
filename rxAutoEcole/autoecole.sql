d8862b5faa83afb4ecf9ec483b57e8a0e9fe82ee
RoonyxFR <80361947+RoonyxFR@users.noreply.github.com>
2022-06-06 18:39:58 +0200

    Create autoecole.sql

---
+++ rxAutoEcole/autoecole.sql
@@ -0,0 +1,7 @@
+INSERT INTO `licenses` (`type`, `label`) VALUES
+('boat', 'Boat License'),
+('dmv', 'Code de la route'),
+('drive', 'Permis de conduire'),
+('drive_bike', 'Permis moto'),
+('drive_truck', 'Permis camion'),
+('weapon', 'Permis de port d\'arme');
