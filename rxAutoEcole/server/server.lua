03ae13799ec660034886946525c86d8474b72d2d
RoonyxFR <80361947+RoonyxFR@users.noreply.github.com>
2022-06-08 18:20:52 +0200

    Update server.lua

--- rxAutoEcole/server/server.lua
+++ rxAutoEcole/server/server.lua
@@ -2,7 +2,6 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-
 -----------------------------------------------> Event acheter code <-----------------------------------------------
 
 RegisterNetEvent("rx:BuyLicense")
@@ -18,7 +17,6 @@ AddEventHandler("rx:BuyLicense", function(price)
     end
 end)
 
-
 RegisterNetEvent("rx:BuyPermis")
 AddEventHandler("rx:BuyPermis", function(price, type)
     local xPlayer = ESX.GetPlayerFromId(source)
@@ -31,3 +29,36 @@ AddEventHandler("rx:BuyPermis", function(price, type)
         TriggerClientEvent('esx:showNotification', source, ("[~r~Erreur~s~] Vous n'avez pas assez d'argent il vous manque ~r~%s$~s~."):format(price - xPlayer.getMoney()))
     end
 end)
+
+-----------------------------------------------> Event License <-----------------------------------------------
+
+RegisterNetEvent("rx:addLicense")
+AddEventHandler("rx:addLicense", function(type)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    MySQL.Async.execute("INSERT INTO user_licenses (type, owner) VALUES (@type, @owner)", {
+        ['@type'] = type,
+        ['@owner'] = xPlayer.identifier
+    })
+end)
+
+ESX.RegisterServerCallback('rx:checkLicense', function(source, cb, target, type)
+    CheckLicense(target, type, cb)
+end)
+
+function CheckLicense(target, type, cb)
+    local identifier = ESX.GetPlayerFromId(target).identifier
+    print(identifier)
+
+    MySQL.Async.fetchAll('SELECT COUNT(*) as count FROM user_licenses WHERE type = @type AND owner = @owner', {
+        ['@type']  = type,
+        ['@owner'] = identifier
+    }, function(result)
+        if tonumber(result[1].count) > 0 then
+            cb(true)
+        else
+            cb(false)
+        end
+
+    end)
+end
